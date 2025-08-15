400b45c7d1e39c99ed32125fa6d4e425959675b4
Jason Chambers <jason.d.chambers@gmail.com>
2025-08-15 11:52:28 -0400

    Support macOs

--- install.sh
+++ install.sh
@@ -2,23 +2,8 @@
 
 # Installation script for config-alacritty
 
-omarchy_install() {
-  # With Omarchy, it is possible to switch the theme so we just
-  # need to point the theme.lua to whatever Omarchy is using
-  echo "This is Arch Linux."
-  if [ -f ~/.config/omarchy/current/theme/alacritty.toml ]; then
-    echo "This is Omarchy"
-  else
-    echo "It looks like Omarchy has not been installed"
-  fi
-}
-
-non_omarchy_install() {
-  echo "I only use Alacritty on Omarchy for now - exiting"
-  exit 1
-}
-
 point_alacritty_to_this_config() {
+  config_file=$1
   echo "Pointing alacritty to this config"
   if [ -L ~/.config/alacritty ]; then
     echo "Alacritty config already set - verifying it points to this config"
@@ -30,25 +15,32 @@ point_alacritty_to_this_config() {
       echo "Alacritty is pointing to another config: ${actual_resolved_path}"
       exit 1
     fi
+    if [ -L alacritty.toml ]; then
+      rm alacritty.toml
+    fi
+    ln -s ${config_file} alacritty.toml
   else
     if [ -d ~/.config/alacritty ]; then
       echo "Alacritty config already exists - moving it to backup"
       mv ~/.config/alacritty{,.bak}
     fi
     echo "Linking Alacritty to this config"
-    ln -s ~/repos/config-alacritty ~/.config/alacritty
+    ln -s $(pwd) ~/.config/alacritty
   fi
 }
 
 main() {
 
-  if [ -f alacritty.toml ]; then
+  if [ -f alacritty.toml.omarchy ]; then
     if [ -f /etc/arch-release ]; then
-      omarchy_install
+      if [ -f ~/.config/omarchy/current/theme/alacritty.toml ]; then
+        echo "Installing for Omarchy"
+        point_alacritty_to_this_config alacritty.toml.omarchy
+      fi
     else
-      non_omarchy_install
+      echo "Installing for macOS"
+      point_alacritty_to_this_config alacritty.toml.macos
     fi
-    point_alacritty_to_this_config
   else
     echo "You need to run this script from within the config-alacritty directory"
   fi
